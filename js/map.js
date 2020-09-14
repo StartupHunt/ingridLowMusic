d809f40e756dcb3c67bb9dd279e22b9b954e2084
Alejandro <alejandro@cafedelossentidos.com>
2020-09-14 10:53:42 -0500

    Testing some shop functionality

--- js/map.js
+++ js/map.js
@@ -1,11 +1,11 @@
 function initMap() {
-  // The location of Uluru
+  // The location of Taxicab
   var taxicab = { lat: 19.4299836, lng: -99.1566844 };
-  // The map, centered at Uluru
+  // The map, centered at Taxicab
   var map = new google.maps.Map(document.getElementById("google-map"), {
     zoom: 20,
     center: taxicab,
   });
-  // The marker, positioned at Uluru
+  // The marker, positioned at Taxicab
   var marker = new google.maps.Marker({ position: taxicab, map: map });
 }
